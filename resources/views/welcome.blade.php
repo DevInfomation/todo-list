cbf7b4d331f94a3227e30d8ae964f8b3d4560e17
DevInfo <devinformationproject@gmail.com>
2025-02-16 19:12:23 +0100

    just simplifying the whole process

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <title>Test</title>
-</head>
-<body>
+@extends('layouts.default')
 
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-    
-</body>
-</html>
+@section('content')
+
+    <h1>Hello, my name is something</h1>
+
+@endsection
