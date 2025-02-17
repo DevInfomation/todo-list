9ce0a81177c93085f331c6546244a553facf4a43
DevInfo <devinformationproject@gmail.com>
2025-02-17 22:18:05 +0100

    adding the links to where the buttons should lead

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,14 @@
 
     <div class="flex justify-center text-1xl pt-2">
         <div class="mr-2">
-            <button class="p-2 text-lg cursor-pointer bg-blue-500 rounded-md w-md">Login</button>
+            <a href="login">
+                <button class="p-2 text-lg cursor-pointer bg-blue-500 rounded-md w-md">Login</button>
+            </a>
         </div>
         <div>
-            <button class="p-2 text-lg cursor-pointer bg-blue-500 rounded-md w-md">Register</button>
+            <a href="register">
+                <button class="p-2 text-lg cursor-pointer bg-blue-500 rounded-md w-md">Register</button>
+            </a>
         </div>
     </div>
 
