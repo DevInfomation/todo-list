b91b1f497723a697fc69791ddf6b63f3f31820f1
DevInfo <devinformationproject@gmail.com>
2025-02-17 21:11:46 +0100

    adding the csrf to the form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,6 +9,7 @@
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form class="space-y-6" action="#" method="POST">
+        @csrf
         <div>
             <label for="email" class="block text-sm/6 font-medium text-white">Email address</label>
             <div class="mt-2">
