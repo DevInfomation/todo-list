16415a17156d82d02ed913bd3bd213cd8ab72067
DevInfo <devinformationproject@gmail.com>
2025-02-17 20:51:48 +0100

    adding text-white since I wanted to changes the appearance of the page to a darker colour

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <!-- resources/views/partials/header.blade.php -->
 
-<div class="mx-auto text-7xl text-center">
+<div class="mx-auto text-7xl text-center text-white">
     <h1>Todo List</h1>
 </div>
