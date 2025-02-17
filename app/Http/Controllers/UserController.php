aac0cf59f25c777eef23024e70fdae6352f3211a
DevInfo <devinformationproject@gmail.com>
2025-02-17 21:26:10 +0100

    I'm creating a user controller when the person logs in they can see the main page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'email' => ['required', 'min3',]
+        ]);
+    }
 }
