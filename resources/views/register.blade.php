f2a36e5f16be1da29cc8c39fb9d9088b204deba1
DevInfo <devinformationproject@gmail.com>
2025-02-17 22:30:39 +0100

    coming up with the inputs.

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,10 +4,16 @@
 
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
+        <h2 class="mt-1 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
     </div>
     
-    
+    <form action="register" method="POST">
+        @csrf
+
+        <input type="text" name="name" placeholder="Enter name..."  class="">
+        <input type="text" name="email" placeholder="Enter email..."  class="">
+        <input type="password" name="password" placeholder="Enter password..."  class="">
+    </form>
 
 
 @endsection
