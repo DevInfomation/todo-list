cef3911ffe428ea667817eabbf164ee735351cee
DevInfo <devinformationproject@gmail.com>
2025-02-17 20:51:26 +0100

    adding some chages to the default blade

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../css/app.css">
     @vite('resources/css/app.css')
     <title>Todo List</title>
 </head>
 <body>
 
-    <header class="p-[3rem] bg-blue-300 font-sans tracking-[0.25em]">
+    <header class="p-[3rem] font-sans tracking-[0.25em]">
         @include('partials.header')
     </header>
 
