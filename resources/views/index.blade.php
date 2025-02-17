4113f69f2a3c15a5744ef3fe006f7cd8a82a8e40
DevInfo <devinformationproject@gmail.com>
2025-02-17 22:13:30 +0100

    Styling the buttons for the login and the register

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,5 +2,15 @@
 
 @section('content')
 
+    <div class="flex justify-center text-2xl">Welcome to the Todo List</div>
+
+    <div class="flex justify-center text-1xl pt-2">
+        <div class="mr-2">
+            <button class="p-2 text-lg cursor-pointer bg-blue-500 rounded-md w-md">Login</button>
+        </div>
+        <div>
+            <button class="p-2 text-lg cursor-pointer bg-blue-500 rounded-md w-md">Register</button>
+        </div>
+    </div>
 
 @endsection
