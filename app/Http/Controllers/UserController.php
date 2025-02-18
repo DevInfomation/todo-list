1c8d3c1396e307ab2e69dad3538e102d851c288e
DevInfo <devinformationproject@gmail.com>
2025-02-18 20:01:14 +0100

    Created the register and added the user to the database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request) {
         $incomingFields = $request->validate([
-            'email' => ['required', 'min3',]
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:3', 'max:200'],
         ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        auth()->login($user);
+
+        return redirect('/main');
+    }
+
+    public function login(Request $request) {
+
     }
 }
