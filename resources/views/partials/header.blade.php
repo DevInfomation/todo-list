9f0a3be3a09fbdb4194f7d9e742a26694f292b28
Stefan <stefan.milkovski@generadevelopment.com>
2025-02-21 10:07:49 +0100

    testing something out

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,3 +3,11 @@
 <div class="mx-auto text-7xl text-center text-white">
     <h1>Todo List</h1>
 </div>
+
+<div>
+    @if (!Auth::user()) 
+        <p>You're currently not signed in</p> 
+    @endif
+
+
+</div>
