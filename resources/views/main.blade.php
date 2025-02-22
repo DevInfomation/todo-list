e470876728b5b745bfe8418287789f2809815b06
DevInfo <devinformationproject@gmail.com>
2025-02-22 13:45:32 +0100

    The logout button

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,4 +8,10 @@
 
     <h1 class="text-4xl ml-10 mt-10">Enter your task here:</h1>
 
+    <form action="/logout" method="POST">
+        @csrf
+
+        <button>Logout</button>
+    </form>
+
 @endsection
