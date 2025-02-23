82fb616f7fc8d7c0921754fb896720d99ed75967
DevInfo <devinformationproject@gmail.com>
2025-02-23 16:52:07 +0100

    I don't think I will be needing this

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,11 +2,8 @@
 
 @section('content')
 
-    @auth
-        <p>Congrats, you have logged in: {{$user->name}}</p>
-    @endauth
-
     <h1 class="text-4xl ml-10 mt-10">Enter your task here:</h1>
+    
 
     <form action="/logout" method="POST">
         @csrf
