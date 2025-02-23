f08dadac7142c95b2128a383d7862c3ea0ad77b4
DevInfo <devinformationproject@gmail.com>
2025-02-23 22:48:39 +0100

    Creating a create task in the user controller and also making changes in the main file

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,11 +6,11 @@
         
         <div class="border-4 border-solid rounded-md border-sky-500 mt-5">
             <h1 class="text-4xl ml-10 mt-10">Enter your task here:</h1>
-            <form action="#">
+            <form action="create-task" method="POST">
                 <div class="flex flex-col items-center justify-content ">
                     @csrf
                     <input name="title" type="text" placeholder="Enter your title here" class="mt-4 mb-4 border rounded p-2">
-                    <input name="description" type="textarea" placeholder="Enter your description here" class="mt-4 mb-4 border rounded p-2">
+                    <textarea name="description" placeholder="Enter your description..." class="mt-4 mb-4 border rounded p-2"></textarea>
                     <button class="mt-7 mb-7 border rounded p-2 bg-blue-900 hover:bg-sky-700 transition duration-300 text-xl">Create Task</button>
                 </div>
             </form>
