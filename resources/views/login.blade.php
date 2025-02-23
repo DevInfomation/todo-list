df5ee73c6de65ddb61ba1aac54876bd5700eedb6
DevInfo <devinformationproject@gmail.com>
2025-02-23 15:22:51 +0100

    I figured it out! It was the name of the inputs that I had to match from the user controller

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,13 +7,17 @@
         <h2 class="mt-1 text-center text-2xl/9 font-bold tracking-tight text-white mb-4">Log in to your account</h2>
     </div>
     
-    <form action="register" method="POST">
+    <form action="{{ route('login.submit') }}" method="POST">
         @csrf
 
         <div class="flex flex-col items-center justify-content min-h-screen">
-            <input type="text" name="name" placeholder="Enter name..."  class="mt-4 mb-4 border rounded p-2">
-            <input type="password" name="password" placeholder="Enter password..."  class="border rounded p-2">
+            <input type="text" name="loginname" placeholder="Enter name..."  class="mt-4 mb-4 border rounded p-2">
+            <input type="password" name="loginpassword" placeholder="Enter password..."  class="border rounded p-2">
             <button class="mt-7 border rounded p-2 bg-blue-900 hover:bg-sky-700 transition duration-300 text-xl">Submit</button>
+            @error('loginname')
+                <div class="text-red-500 mb-4">{{ $message }}</div>
+                {{-- {{dd($message)}} --}}
+            @enderror
         </div>
     </form>
 
