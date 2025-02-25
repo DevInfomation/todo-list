3dbedc862dfb8dbc33184c28b154e505f0727c76
Stefan <stefan.milkovski@generadevelopment.com>
2025-02-25 16:27:20 +0100

    adding the table name and also changing the body to description

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,9 +9,12 @@ class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'body', 'user_id'];
+    protected $table = 'tasks';
+
+    protected $fillable = ['title', 'description', 'user_id'];
 
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
     }
+
 }
